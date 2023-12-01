29fb67e8dd45cde4d2ce37d2c48fc4ccb554ef16
Stephen Zhao <xinghaozhaoex@gmail.com>
2023-11-30 19:19:23 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim import Adam
+import tqdm
+import gstools as gs
