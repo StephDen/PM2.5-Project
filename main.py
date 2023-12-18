5ae6e6a61a0e6b19c7377ed6dadc4f52d227ee40
Stephen Zhao <xinghaozhaoex@gmail.com>
2023-12-18 09:07:27 -0800

    CNN Update

--- main.py
+++ main.py
@@ -4,81 +4,75 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from torch.optim import Adam
 from torch.utils.data import Dataset
+import torch.nn.init as init
 import pandas as pd
 import utm
 import os
 import random
+from sklearn.preprocessing import MinMaxScaler
 
 # Set script directory to working directory
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 #%%
 class  TrainingDataset ():
     def __init__(self, tensor_size):
-        
-        """ # Function to apply the conversion to each row
-        def lat_lon_to_utm(row):
-            utm_coords = utm.from_latlon(row["lat"],row["lon"])
-            return utm_coords[0], utm_coords[1]
-        # Return a new dataframe with the rounded UTM coordinates
-        def lat_long_to_utm_df(df):
-            df[['utm_easting', 'utm_northing']] = df.apply(lat_lon_to_utm, axis=1, result_type='expand')
-            df["utm_easting"] = df["utm_easting"].round(-3)
-            df["utm_northing"] = df["utm_northing"].round(-3)
-            return df
-        #pm25
-        self.pm25 = lat_long_to_utm_df(pd.read_csv("PM25.csv")[["value","lat","lon"]])
-        #modisaod
-        self.modisaod = lat_long_to_utm_df(pd.read_csv("MAIACAOD.csv")[["lat","lon","value"]])
-        #era5
-        era5 = pd.read_csv("ERA5data.csv")
-        self.U_windspeed = lat_long_to_utm_df(era5[["lat","lon","U_windspeed"]])
-        self.V_windspeed = lat_long_to_utm_df(era5[["lat","lon","V_windspeed"]])
-        self.DewpointTemp = lat_long_to_utm_df(era5[["lat","lon","DewpointTemp"]])
-        self.Temp = lat_long_to_utm_df(era5[["lat","lon","Temp"]])
-        self.SurfPressure = lat_long_to_utm_df(era5[["lat","lon","SurfPressure"]])
-        self.Precip = lat_long_to_utm_df(era5[["lat","lon","Precip"]])
-        """
+        def custom_min_max_scaling(dataframe, columns=None):
+            """
+            Normalize specified columns of a DataFrame using Min-Max scaling.
+
+            Parameters:
+            - dataframe (pd.DataFrame): The DataFrame containing the data.
+            - columns (list): List of columns to be normalized. If None, normalize all numeric columns.
+
+            Returns:
+            - pd.DataFrame: The DataFrame with normalized values.
+            - MinMaxScaler: The fitted scaler object.
+            """
+            if columns is None:
+                # If columns are not specified, normalize all numeric columns
+                numeric_columns = dataframe.select_dtypes(include=['float64']).columns
+            else:
+                numeric_columns = columns
+
+            # Initialize the scaler
+            scaler = MinMaxScaler()
+
+            # Fit the scaler on the specified columns and transform the data
+            dataframe[numeric_columns] = scaler.fit_transform(dataframe[numeric_columns])
+
+            return dataframe, scaler
+
         self.tensor_size = tensor_size
-        self.pm25 = pd.read_csv("PM25.csv")
-        self.modisaod = pd.read_csv("MAIACAOD.csv")
-        self.ERA5data = pd.read_csv("ERA5data.csv")
-        self.U_windspeed
+        self.pm25, self.pm25_scaler = custom_min_max_scaling(pd.read_csv("PM25_final.csv"))
+        self.modisaod, self.modisaod_scaler = custom_min_max_scaling(pd.read_csv("MAIACAOD.csv"))
+        self.U_windspeed, self.U_windspeed_scaler = custom_min_max_scaling(pd.read_csv("U_windspeed.csv"))
+        self.V_windspeed, self.V_windspeed_scaler = custom_min_max_scaling(pd.read_csv("V_windspeed.csv"))
+        self.DewpointTemp, self.DewpointTemp_scaler = custom_min_max_scaling(pd.read_csv("DewpointTemp.csv"))
+        self.Temp, self.Temp_scaler = custom_min_max_scaling(pd.read_csv("Temp.csv"))
+        self.SurfPressure, self.SurfPressure_scaler = custom_min_max_scaling(pd.read_csv("SurfPressure.csv"))
+        self.Precip, self.Precip_scaler = custom_min_max_scaling(pd.read_csv("Precip.csv"))
+    
+    def datasearch(self, df, lat, lon):
+        lat_max = int(lat + self.tensor_size/2)
+        lat_min = int(lat - self.tensor_size/2)
+        lon_max = int(lon + self.tensor_size/2)
+        lon_min = int(lon - self.tensor_size/2)
 
+        filtered_data = df.iloc[lat_min:lat_max, lon_min:lon_max]
 
-
-    # Convert UTM data to a tensor
-"""     def datatotensor(self,df,easting_max,easting_min,northing_max,northing_min):
-        # Filter the dataframe to the specified easting and northing ranges
-        filtered_data = df[
-            (df['utm_easting'] >= easting_min) & (df['utm_easting'] <= easting_max) &
-            (df['utm_northing'] >= northing_min) & (df['utm_northing'] <= northing_max)
-        ]
-
-        kilometer_spacing = 1  # Assuming a regular grid
-        result_tensor = torch.zeros((tensor_size,tensor_size),dtype=torch.float32)
-        # Populate the tensor with values from sparse data
-        for index, row in filtered_data.iterrows():
-            easting_index = int((row['utm_easting'] - easting_min)/kilometer_spacing)
-            northing_index = int((row['utm_northing'] - northing_min)/kilometer_spacing)
-            
-            # Ensure that the indices are within the tensor size
-            if 0 <= easting_index < self.tensor_size and 0 <= northing_index < self.tensor_size:
-                result_tensor[easting_index, northing_index] = row['value']
-
-        return result_tensor """
-    def datasearch(self, df, lat,lon):
-        lat_max = lat + self.tensor_size/2
-        lat_min = lat - self.tensor_size/2
-        lon_max = lon + self.tensor_size/2
-        lon_min = lon - self.tensor_size/2
-
-        filtered_data = df.iloc[lat_min:lat_max,lon_min:lon_max,]
-
-        result_tensor = torch.zeros((tensor_size,tensor_size),dtype=torch.float32)
-        for lat in range(lat_min, lat_max):
-            for lon in range(lon_min, lon_max):
-                result_tensor[lat, lon] = filtered_data[lat, lon]
+        result_tensor = torch.zeros((self.tensor_size, self.tensor_size), dtype=torch.float32)
+        
+        # Check if filtered_data has at least one row and one column
+        if len(filtered_data) > 0 and len(filtered_data.columns) > 0:
+            for i in range(lat_min, lat_max):
+                if (i > -1 and i < len(filtered_data)):
+                    for j in range(lon_min, lon_max):
+                        if (j > -1 and j < len(filtered_data.columns)):
+                            # Convert the value to a numeric type (float)
+                            result_tensor[i - lat_min, j - lon_min] = pd.to_numeric(filtered_data.iloc[i - lat_min, j - lon_min], errors='coerce')
+        
         return result_tensor
+
 
     def getdata(self, lat,lon):
         modisaod = self.datasearch(self.modisaod, lat,lon)
@@ -157,12 +151,16 @@ class CNN(nn.Module):
         
         # Define the layers
         self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=3, stride=1, padding=1)
-        self.relu = nn.ReLU()
+        self.relu = nn.LeakyReLU(negative_slope=0.01)
         self.maxpool = nn.MaxPool2d(kernel_size=2)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.fc1 = nn.Linear(64*2*2, 128)
         self.fc2 = nn.Linear(128, 1)
-
+        self.init_weights()
+    def init_weights(self):
+        for module in self.modules():
+            if isinstance(module, nn.Linear):
+                init.xavier_uniform_(module.weight)
     def forward(self, x):
         x = self.conv1(x)
         x = self.relu(x)
@@ -181,12 +179,12 @@ class CNN(nn.Module):
 #%%
 # Initialize U-Net model, loss function, and optimizer
 in_channels = 7  # Number of input channels
-num_epochs = 1000
+num_epochs = 100
 model = CNN(in_channels)
 
 criterion = nn.MSELoss()
 
-optimizer = Adam(model.parameters(), lr=0.001)  # Adjust the learning rate as needed
+optimizer = Adam(model.parameters(), lr=0.0001, weight_decay=1e-5)  # Adjust the learning rate as needed
 
 
 for epoch in range(num_epochs):
@@ -196,16 +194,17 @@ for epoch in range(num_epochs):
 
     model.train()
 
-    inputs = dataset.getdata(row['utm_northing'],row['utm_easting'])
+    inputs = dataset.getdata(int(row['row']),int(row['col']))
     targets = row['value']
 
     optimizer.zero_grad()
     outputs = model(inputs)
     loss = criterion(outputs, torch.tensor(targets).float())
     loss.backward()
+    torch.nn.utils.clip_grad_norm_(model.parameters(), 5)
     optimizer.step()
 
     
-    print(f"Epoch {epoch+1}, Loss: {loss}")
+    print(f"Epoch {epoch+1}, Loss: {loss}, Target: {targets}, Output: {outputs}")
 
 # %%
