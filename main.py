c12f0fb5c279d45503b6ca921c2231aa55cad3e8
Stephen Zhao <xinghaozhaoex@gmail.com>
2023-12-21 22:08:05 -0800

    resnet

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from torch.utils.data import Dataset
 import torch.nn.init as init
 import torch.nn.functional as F
 from torchvision.transforms import functional as TF
+import torchvision
 import pandas as pd
 import os
 import random
@@ -33,7 +34,7 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 #%%
 class  TrainingDataset ():
     def __init__(self, tensor_size):
-        def custom_min_max_scaling(dataframes, columns):
+        def custom_min_max_scaling(dataframes, columns = None):
             """
             Normalize specified columns of a DataFrame using Min-Max scaling.
 
@@ -45,12 +46,21 @@ class  TrainingDataset ():
             - pd.DataFrame: The DataFrame with normalized values.
             - MinMaxScaler: The fitted scaler object.
             """
+            if columns is None:
+                # If columns are not specified, normalize all numeric columns
+                numeric_columns = dataframes[0].select_dtypes(include=['float64']).columns
+            else:
+                numeric_columns = columns
 
-            # Initialize the scaler
+            # Concatenate only the numeric columns of all DataFrames in the list
+            combined_numeric_df = pd.concat([df[numeric_columns] for df in dataframes], ignore_index=True)
+
+            # Initialize the scaler and fit it to the combined numeric data
             scaler = MinMaxScaler()
+            scaler.fit(combined_numeric_df)
 
             # Apply min-max scaling to each DataFrame in the list
-            scaled_dataframes = [pd.DataFrame(scaler.fit_transform(df, columns), columns=df.columns) for df in dataframes]
+            scaled_dataframes = [pd.DataFrame(scaler.transform(df[numeric_columns]), columns=numeric_columns) for df in dataframes]
 
             return scaled_dataframes, scaler
         def read_csvs(file_name, directory_path = "data"):
@@ -63,21 +73,22 @@ class  TrainingDataset ():
             folders = [entry for entry in files_folders if os.path.isdir(os.path.join(directory_path, entry))]
             dataframes = []
             for folder in folders:
-                file_path = os.path.join(folder, file_name)
+                file_path = os.path.join(directory_path, folder, file_name)
+                print(file_path)
                 df = pd.read_csv(file_path)
                 dataframes.append(df)
             return dataframes
         
         root_folder = "data"
         self.tensor_size = tensor_size
-        self.pm25, self.pm25_scaler = custom_min_max_scaling(read_csvs(root_folder, "PM25.csv"), columns=['value'])
-        self.modisaod, self.modisaod_scaler = custom_min_max_scaling(read_csvs(root_folder, "AODregrid.csv")) #size: 47,48
-        self.U_windspeed, self.U_windspeed_scaler = custom_min_max_scaling(read_csvs(root_folder, "U_windspeed.csv"))
-        self.V_windspeed, self.V_windspeed_scaler = custom_min_max_scaling(read_csvs(root_folder, "V_windspeed.csv"))
-        self.DewpointTemp, self.DewpointTemp_scaler = custom_min_max_scaling(read_csvs(root_folder, "DewpointTemp.csv"))
-        self.Temp, self.Temp_scaler = custom_min_max_scaling(read_csvs(root_folder, "Temp.csv"))
-        self.SurfPressure, self.SurfPressure_scaler = custom_min_max_scaling(read_csvs(root_folder, "SurfPressure.csv"))
-        self.Precip, self.Precip_scaler = custom_min_max_scaling(read_csvs(root_folder, "Precip.csv"))
+        self.pm25, self.pm25_scaler = custom_min_max_scaling(read_csvs("PM25.csv",root_folder), columns=['value','col','row'])
+        self.modisaod, self.modisaod_scaler = custom_min_max_scaling(read_csvs("AODregrid.csv",root_folder)) #size: 47,48
+        self.U_windspeed, self.U_windspeed_scaler = custom_min_max_scaling(read_csvs("U_windspeed.csv",root_folder))
+        self.V_windspeed, self.V_windspeed_scaler = custom_min_max_scaling(read_csvs("V_windspeed.csv",root_folder))
+        self.DewpointTemp, self.DewpointTemp_scaler = custom_min_max_scaling(read_csvs("DewpointTemp.csv",root_folder))
+        self.Temp, self.Temp_scaler = custom_min_max_scaling(read_csvs("Temp.csv",root_folder))
+        self.SurfPressure, self.SurfPressure_scaler = custom_min_max_scaling(read_csvs("SurfPressure.csv",root_folder))
+        self.Precip, self.Precip_scaler = custom_min_max_scaling(read_csvs("Precip.csv",root_folder))
         # self.pm25, self.pm25_scaler = custom_min_max_scaling(pd.read_csv("PM25_final.csv"), columns=['value'])
         # self.modisaod, self.modisaod_scaler = custom_min_max_scaling(pd.read_csv("AODregrid.csv")) #size: 47,48
         # self.U_windspeed, self.U_windspeed_scaler = custom_min_max_scaling(pd.read_csv("U_windspeed.csv"))
@@ -215,24 +226,130 @@ class SimpleCNN(nn.Module):
         x = torch.sigmoid(x)
         x = x.squeeze(1)
         return x
+class AdvancedCNN(nn.Module):
+    def __init__(self, in_channels):
+        super(AdvancedCNN, self).__init__()
+
+        # Define the layers
+        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=2)
+        self.dropout = nn.Dropout(0.5)
+
+        self.fc1 = nn.Linear(1, 256)
+        self.fc2 = nn.Linear(256, 1)
+
+        self.init_weights()
+
+    def init_weights(self):
+        for module in self.modules():
+            if isinstance(module, nn.Linear) or isinstance(module, nn.Conv2d):
+                nn.init.xavier_uniform_(module.weight)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.conv3(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        #x = x.view(128)  # Flatten before fully connected layers
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = torch.sigmoid(x)
+
+        return x.squeeze(1)
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        # Modified the first convolution layer to accept 7x10x10 input
+        self.conv1 = nn.Conv2d(7, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        
+        # Use global average pooling
+        out = F.adaptive_avg_pool2d(out, (1, 1))
+        
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+def ResNet18_7Channels():
+    return ResNet(BasicBlock, [2,2,2,2])
 # Initialize CNN model, loss function, and optimizer
 in_channels = 7  # Number of input channels
-num_epochs = 100
-model = SimpleCNN(in_channels)
+num_epochs = 10000
+model = ResNet18_7Channels()
 
 criterion = nn.MSELoss()
 
-optimizer = Adam(model.parameters(), lr=0.001, weight_decay=1e-3)  # Adjust the learning rate as needed
+optimizer = Adam(model.parameters(), lr=0.01, weight_decay=1e-3)  # Adjust the learning rate as needed
 
 losses = []
 for epoch in range(num_epochs):
     random_folder = random.randint(0, len(dataset.pm25) - 1)
-    random_index = random.randint(0, len(dataset.pm25) - 1)
-    row = dataset.pm25.iloc[random_index]
+    random_index = random.randint(0, len(dataset.pm25[random_folder]) - 1)
+    row = dataset.pm25[random_folder].iloc[random_index]
 
     model.train()
 
     inputs = dataset.getdata(random_folder,int(row['row']),int(row['col']))
+    inputs = torch.unsqueeze(inputs, 0)
     targets = row['value']
 
     optimizer.zero_grad()
